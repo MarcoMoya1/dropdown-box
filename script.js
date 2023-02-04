554ed88e56576a1a593db55161f2e0e828ede0da
915marco <915marco@gmail.com>
2023-02-04 16:58:08 -0600

    js working

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const dropdownBtn = document.getElementById("btn");
-
 const dropdownMenu = document.getElementById("dropdown");
-
 const toggleArrow = document.getElementById("arrow");
 
-const toggleDropdown = funtion () {
-    dropdownMenu.classList.toggle("show");
-    toggleArrow.classList.toggle("arrow");
+// Toggle dropdown function
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
 };
 
-dropdownBtn.addEventListener("click", funtion (e) {
-    e.stopPropagation();
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
     toggleDropdown();
+  }
 });
